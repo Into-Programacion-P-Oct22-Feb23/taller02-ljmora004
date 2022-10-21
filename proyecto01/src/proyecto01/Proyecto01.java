df6e4e93a1e2531ccc14a38f1d2132dd4a61f816
Luis Mora <ljmora1@utpl.edu.ec>
2022-10-21 11:56:08 -0500

    primer commit

---
+++ proyecto01/src/proyecto01/Proyecto01.java
@@ -0,0 +1,12 @@
+
+package proyecto01;
+
+public class Proyecto01 {
+
+    public static void main(String[] args) {
+      System.out.println("Ejemplo básico de Java.");
+      System.out.println("Versionado a través de Git.");
+      System.out.println("Manejado desde GitHub.");
+    }
+    
+}
